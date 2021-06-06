7cdf3bb74230f5f048bb910f2bc02c4cebf716ba
Reckon Mazumdar <reckonmazumdar12@gmail.com>
2021-06-06 09:46:03 +0530

    recursion complete2

--- DSA/Recursion/Intermediate/Subset sums.cpp
+++ DSA/Recursion/Intermediate/Subset sums.cpp
@@ -1,18 +1,20 @@
  //https://practice.geeksforgeeks.org/problems/subset-sums2234/1
  //https://www.youtube.com/watch?v=rYkfBRtMJr8
- void funsubset(vector<int>&ans,vector<int>arr,int l,int sum,int N){
-        if(l==N){
-            ans.push_back(sum);
-            return;
-        }
-        funsubset(ans,arr,l+1,sum+arr[l],N);
-        funsubset(ans,arr,l+1,sum,N);
-    }
+ void funsubset(vector<int>&ans, vector<int>arr, int sum, int N) {
+	if (N == 0) {
+		ans.push_back(sum);
+		return;
+	}
+	int sum1 = sum;
+	int sum2 = sum + arr[N - 1];
+	funsubset(ans, arr, sum1, N - 1);
+	funsubset(ans, arr, sum2, N - 1);
+}
     vector<int> subsetSums(vector<int> arr, int N)
     {
         // Write Your Code here
        vector<int>ans;
-       funsubset(ans,arr,0,0,N);
+       funsubset(ans,arr,0,N);
        sort(ans.begin(),ans.end());
        return ans;
         
